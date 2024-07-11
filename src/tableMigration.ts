464739ac753a880a10d6b910f838816318d3998a
Darker-Ink <rstonehouse66@gmail.com>
2024-07-11 18:40:38 -0500

    feat: migrate stuff

---
+++ src/tableMigration.ts
@@ -0,0 +1,20 @@
+import ConfigManager from "./Utils/Classes/ConfigManager.ts";
+import * as _ from "@/Utils/Cql/Tables/Tables.ts";
+import Client from "./Utils/Classes/DB/Client.ts";
+
+const cfg = new ConfigManager()
+
+await cfg.load();
+
+const config = cfg.config!;
+
+await Client.getInstance().connect({
+    keyspace: config.scyllaDB.keyspace,
+    nodes: config.scyllaDB.nodes,
+    password: config.scyllaDB.password,
+    username: config.scyllaDB.username,
+    db: {
+        durableWrites: config.scyllaDB.durableWrites,
+        networkTopologyStrategy: config.scyllaDB.networkTopologyStrategy
+    }
+})
